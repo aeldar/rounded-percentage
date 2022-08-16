3811fb50de3bd2bd9c42af53b09ba1c2b2627d9b
aeldar <eldar.aliyev8@gmail.com>
2022-08-17 00:44:04 +0500

    improve performance

--- src/rounded-percentage.ts
+++ src/rounded-percentage.ts
@@ -14,16 +14,15 @@ export function roundedPercentage<T>(selector: (x: T) => number, xs: T[]): [T, n
 
   // distribute the difference between the rounded values in decreasing order of their decimal parts
   const { result } = sortedByDecimalPart.reduce(
-    ({ difference, result }, curr) =>
-      difference > 0
-        ? {
-            difference: difference - 1,
-            result: [...result, [curr, Math.floor(selector(curr)) + 1] as [T, number]],
-          }
-        : {
-            difference: 0,
-            result: [...result, [curr, Math.floor(selector(curr))] as [T, number]],
-          },
+    (acc, curr) => {
+      if (acc.difference > 0) {
+        acc.difference -= 1;
+        acc.result.push([curr, Math.floor(selector(curr)) + 1]);
+      } else {
+        acc.result.push([curr, Math.floor(selector(curr))]);
+      }
+      return acc;
+    },
     { difference, result: [] as [T, number][] },
   );
 
